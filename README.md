3861629cfae8c476680400d5f2621ee5f56c03be
olly-evans <Olly.Evans@warwick.ac.uk>
2025-09-15 18:17:53 +0100

    played about with drawing # to screen for border.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notes
+
+- Got rectangle working. But it wont suffice. Need to get it to work with erow struct.
+- Also need to make rectangle one less row than it should be for a help bar at the bottom for controls.
+- Create the struct like in kilo.c and get it to work for one row.
--- src/output.c
+++ src/output.c
@@ -10,13 +10,13 @@ void dashRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 4);
 
     dashDrawRows(&ab);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", dashCon.cy + 1, dashCon.cx + 1);
-    abAppend(&ab, buf, strlen(buf));
+    abAppend(&ab, buf, strlen(buf)); // move cursor. maybe movement stuff here.
 
     abAppend(&ab, "\x1b[?25h", 6);
     
@@ -26,13 +26,24 @@ void dashRefreshScreen() {
 }
 
 void dashDrawRows(struct abuf *ab) {
+
     int y;
-    for (int y = 0; y < dashCon.dashrows; y++) {
-        abAppend(ab, "~", 1);
+    int x;
+    abAppend(ab, "\x1b[3J", 4); // Clear Scrollback buffer
+    for (y = 0; y < dashCon.dashrows; y++) {
         
-        abAppend(ab, "\x1b[K", 3);
-        if (y < dashCon.dashrows - 1) {
-            abAppend(ab, "\r\n", 2);
+        abAppend(ab, "\x1b[K", 3); // Clear line.
+
+        for (x = 0; x < dashCon.dashcols; x++) {
+            if (y == 0 || y == dashCon.dashrows - 1) {
+                abAppend(ab, "#", 1);
+            } else {
+                // Also, to fill in chars inbetween hashtags might be difficult with this formats.
+                // 999 works for now. May need to make it length.
+                // Probably solved with erow struct check kilo.c
+                abAppend(ab, "#\x1b[999C#", 7);  
+            }
         }
+        if (y < dashCon.dashrows - 1) abAppend(ab, "\r\n", 2); // Don't \n last row.
     }
 }
