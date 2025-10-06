87f26dafb0b84e693a87654a689ff33803a068a0
olly-evans <evansoliver345@gmail.com>
2025-10-06 15:05:42 +0100

    moved gridInit() and freeGrid() to grid.c

--- src/grid.c
+++ src/grid.c
@@ -1,6 +1,10 @@
+#include <stdlib.h>
+#include <limits.h>
+
 #include "abuf.h"
 #include "grid.h"
 #include "cell.h"
+#include "terminal.h"
 
 void drawGrid(struct abuf *ab) {
     
@@ -53,4 +57,48 @@ void drawGrid(struct abuf *ab) {
         }
         if (y < g->rows - 1) abAppend(ab, "\r\n", 2);
     }
+}
+
+struct Grid* initGrid(struct Grid *g, int rows, int cols) {
+	g = malloc(sizeof(struct Grid));
+	if (!g) die("Couldn't allocate memory for grid of terminals size.");
+
+	g->rows = rows;
+	g->cols = cols;
+
+	// Allocate memory for an array of row pointers, sized for rows
+	g->cells = malloc(sizeof(struct Cell*) * rows);
+	if (!g->cells) die("g->cells not allocated.");
+
+	for (int y = 0; y < rows; y++) {
+		// Allocate memory for a row of cells, sized for cols
+		g->cells[y] = malloc(sizeof(struct Cell) * cols);
+		if (!g->cells[y]) die("g->cells[y] not allocated");
+
+		for (int x = 0; x < cols; x++) {
+			if (y == 0 || x == 0 || y == Con.screenrows - 1 || x == Con.screencols - 1) {
+				g->cells[y][x].type = PERMANENT_BARRIER;
+				g->cells[y][x].ch = '#';
+			} else {
+				g->cells[y][x].type = EMPTY;
+				g->cells[y][x].ch = ' ';
+			}
+
+			g->cells[y][x].x = x;
+			g->cells[y][x].y = y;
+			g->cells[y][x].g = INT_MAX;
+			g->cells[y][x].weight = 1;
+			g->cells[y][x].inOpenSet = false;
+			g->cells[y][x].inClosedSet = false;
+		}
+	}
+	return g;
+}
+
+void freeGrid(struct Grid *g) {
+	
+	for (int y = 0; y < g->rows; y++) {
+		free(g->cells[y]);
+	}
+	free(g->cells);
 }
--- src/init.c
+++ src/init.c
@@ -52,50 +52,6 @@ void init() {
 	getTableColWidths();
 }
 
-struct Grid* initGrid(struct Grid *g, int rows, int cols) {
-	g = malloc(sizeof(struct Grid));
-	if (!g) die("Couldn't allocate memory for grid of terminals size.");
-
-	g->rows = rows;
-	g->cols = cols;
-
-	// Allocate memory for an array of row pointers, sized for rows
-	g->cells = malloc(sizeof(struct Cell*) * rows);
-	if (!g->cells) die("g->cells not allocated.");
-
-	for (int y = 0; y < rows; y++) {
-		// Allocate memory for a row of cells, sized for cols
-		g->cells[y] = malloc(sizeof(struct Cell) * cols);
-		if (!g->cells[y]) die("g->cells[y] not allocated");
-
-		for (int x = 0; x < cols; x++) {
-			if (y == 0 || x == 0 || y == Con.screenrows - 1 || x == Con.screencols - 1) {
-				g->cells[y][x].type = PERMANENT_BARRIER;
-				g->cells[y][x].ch = '#';
-			} else {
-				g->cells[y][x].type = EMPTY;
-				g->cells[y][x].ch = ' ';
-			}
-
-			g->cells[y][x].x = x;
-			g->cells[y][x].y = y;
-			g->cells[y][x].g = INT_MAX;
-			g->cells[y][x].weight = 1;
-			g->cells[y][x].inOpenSet = false;
-			g->cells[y][x].inClosedSet = false;
-		}
-	}
-	return g;
-}
-
-void freeGrid(struct Grid *g) {
-	
-	for (int y = 0; y < g->rows; y++) {
-		free(g->cells[y]);
-	}
-	free(g->cells);
-}
-
 void getTableColWidths() {
 	/* Get the widest str in each column. */
 
