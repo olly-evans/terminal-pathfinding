55477c8c7c7f8756d65053d7937444709bc80a0e
olly-evans <evansoliver345@gmail.com>
2025-10-12 19:32:51 +0100

    finished menu tweaking

--- src/menu.c
+++ src/menu.c
@@ -12,15 +12,8 @@
 #include "grid.h"
 #include "menu.h"
 
-/* WELCOME SCREEN */
-
-
-
 void drawMenu() {
-
     Con.state = STATE_WELCOME;
-
-    // Define abuf for this welcome menu.
     struct abuf wel_ab = ABUF_INIT;
 
     abAppend(&wel_ab, "\x1b[?25l", 6);
@@ -35,38 +28,32 @@ void drawMenu() {
     abAppend(&wel_ab, mcursor, strlen(mcursor));
 
     write(STDOUT_FILENO, wel_ab.b, wel_ab.len);
-
-    // if algo chosen in cursor row, proceed.
     abFree(&wel_ab);
 }
-
 
 void drawMenuItems(struct abuf *ab) {
     
     char welcome[80];
-    abAppend(ab, "\x1b[1m", 4);
     int welcomelen = snprintf(welcome, sizeof(welcome), "Welcome to PATH -- Version %s", PATH_VERSION);
-    abAppendCentered(ab, welcome);
-    abAppend(ab, "\x1b[0m", 4);
-    abAppend(ab, "\r\n\r\n", 4);
+    abAppend(ab, "\x1b[1m\x1b[45m", 9);
+    abAppendCentered(ab, welcome); // Append 
+    abAppend(ab, "\x1b[K", 3); // Not great but works.
+    abAppend(ab, "\x1b[0m", 4); // Reset styles.
+    abAppend(ab, "\r\n\r\n", 4); // Double new line.
 
     abAppendCentered(ab, "An in-terminal pathfinding algorithm visualizer!");
     abAppend(ab, "\r\n\r\n", 4);
         
-    
-    // global variable here that keeps algorithmsIdx.
     int padding = (Con.screencols - strlen(M.algorithms[M.algoIdx])) / 2;
     while(padding--) abAppend(ab, " ", 1);
-
     abAppend(ab, "<", 1);
     
-    abAppend(ab, "\x1b[33m", 5);
+    abAppend(ab, "\x1b[36m", 5);
     abAppend(ab, M.algorithms[M.algoIdx], strlen(M.algorithms[M.algoIdx]));
     abAppend(ab, "\x1b[0m", 4);
-    abAppend(ab, ">", 1);
-    
 
+    abAppend(ab, ">", 1);
     abAppend(ab, "\r\n\r\n", 4);
     
-    abAppendCentered(ab, "[ Use the arrow-keys and Enter to select an algorithm ]");
+    abAppendCentered(ab, "[ Use Arrow Keys / Enter to Select ] -- [ Ctrl-Q to Quit ]");
 }
