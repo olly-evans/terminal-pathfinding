9607d4f664ae3306e628ba1909c5cb8026cf99b0
olly-evans <evansoliver345@gmail.com>
2026-09-05 12:47:43 +0100

    started unit testing grid.c

--- test/test_grid.c
+++ test/test_grid.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+#include "grid.h"
+
+/* UNIT */
+
+void test_init_grid() {
+    struct Grid *g = NULL;
+    g = initGrid(g, 10, 10);
+
+    // is y = row is x = col
+    // i guess we can assert all conditions with a for.
+
+    // for (int y = 0; y < 10; y++) {
+    //     for (int x = 0; x < 10; x++) {
+    //         assert(g->cells[y][x].g)
+    //     }
+    //     assert(g->cells[y])
+    // }
+
+    // access g->cells[11][11]
+    
+    return;
+}
+
 int main(void) {
+    test_init_grid();
     return 0;
 }
