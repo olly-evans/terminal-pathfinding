dcfcfe46cd3ccd5d4b95cf1a0eb54f0c1c44e4a9
olly-evans <evansoliver345@gmail.com>
2026-09-06 14:01:27 +0100

    made initGrid just return the initialised grid struct instead of providing an already existing one as arg

--- include/grid.h
+++ include/grid.h
@@ -17,5 +17,6 @@ extern struct Grid *g;
 void drawGrid(struct abuf *ab);
 struct Grid* randomizeGrid(struct Grid *g);
 struct Grid* resetGrid(struct Grid* g);
+struct Grid* initGrid(int rows, int cols);
 
 #endif
--- include/init.h
+++ include/init.h
@@ -2,7 +2,6 @@
 #define INIT_H
 
 void init();
-struct Grid* initGrid(struct Grid *g, int rows, int cols);
 void freeGrid(struct Grid *g);
 
 #endif
