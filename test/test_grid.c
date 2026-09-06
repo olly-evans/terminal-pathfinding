d14a8dddbd21e0071a49219ef494b8ed6d1bb727
olly-evans <evansoliver345@gmail.com>
2026-09-06 17:29:39 +0100

    initGrid test test

--- test/test_grid.c
+++ test/test_grid.c
@@ -7,24 +7,12 @@
 /* UNIT */
 
 void test_init_grid() {
+
     struct Grid *g = initGrid(10, 10);
 
     assert(g != NULL);
     assert(g->rows == 10);
-    assert(g->cols == 10);
-
-    // is y = row is x = col
-    // i guess we can assert all conditions with a for.
-
-    // for (int y = 0; y < 10; y++) {
-    //     for (int x = 0; x < 10; x++) {
-    //         assert(g->cells[y][x].g)
-    //     }
-    //     assert(g->cells[y])
-    // }
-
-    // access g->cells[11][11]
-    
+    assert(g->cols == 10);    
     return;
 }
 
