2f2fa5385b9f9e9930d220057267acc42589fbc5
olly-evans <evansoliver345@gmail.com>
2026-09-08 18:27:30 +0100

    testing drawGrid

--- test/test_grid.c
+++ test/test_grid.c
@@ -5,6 +5,21 @@
 #include "grid.h"
 #include "init.h"
 #include "abuf.h"
+#include "terminal.h"
+
+
+int getSubStrNum(char *full, char *sub) {
+
+    int count = 0;
+    char *pos = full;
+
+    while ((pos = strstr(pos, sub)) != NULL) {
+        count++;
+        pos += strlen(sub);
+    }
+
+    return count;
+}
 
 /* UNIT */
 
@@ -26,30 +41,19 @@ void test_init_grid() {
 
 void test_draw_grid() {
 
-    struct Grid *g = initGrid(10, 10);
+    int rows = 10;
+    int cols = 10;
+
+    struct Grid *g = initGrid(rows, cols);
 
     struct abuf ab = ABUF_INIT;
 
     drawGrid(g, &ab);
     assert(ab.b != NULL);
 
-    // what does drawGrid promise.
-    
-    // 10x10 grid promises 9 carriage returns. n-1 because of last line.
-    
-    int carriageRetCount = 0;
-    char *pos = ab.b;
-    printf("%s", pos);
-    while ((pos = strstr(pos, "\r\n")) != NULL) {
-        printf("posLen: %d", strlen(pos));
-        carriageRetCount++;
-        printf("carriageret: %d\n", carriageRetCount);
-        pos += strlen("\r\n");
-    }
+    assert(getSubStrNum(ab.b, HIDE_CURSOR) == 1);
+    assert(getSubStrNum(ab.b, RESET_F) == rows*cols);
 
-    printf("count: %d\n", carriageRetCount);
-
-    assert(carriageRetCount == 9);
 
     return;
 }
