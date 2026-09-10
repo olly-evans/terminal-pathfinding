e92ca57c5fa15962103d7994c136f236936e9943
olly-evans <evansoliver345@gmail.com>
2026-09-10 16:32:38 +0100

    changes to test_draw_grid()

--- test/test_grid.c
+++ test/test_grid.c
@@ -46,15 +46,15 @@ void test_draw_grid() {
 
     Grid *g = initGrid(rows, cols);
 
-    struct abuf t_ab = ABUF_INIT;
+    struct abuf *t_ab = ABUF_INIT;
 
-    drawGrid(g, &t_ab);
+    drawGrid(g, t_ab);
 
 
-    assert(t_ab.b != NULL);
+    assert(t_ab->b != NULL);
 
-    assert(getSubStrNum(t_ab.b, HIDE_CURSOR) == 1);
-    assert(getSubStrNum(t_ab.b, RESET_F) == rows*cols);
+    assert(getSubStrNum(t_ab->b, HIDE_CURSOR) == 1);
+    assert(getSubStrNum(t_ab->b, RESET_F) == rows*cols);
 
     return;
 }
