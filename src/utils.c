27ab2e861a242edd6aae5fa0e2d53bc1d1ddabe0
olly-evans <evansoliver345@gmail.com>
2026-09-10 13:27:06 +0100

    replaced usleep w nanosleep

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <time.h>   
 
 void die(char *s) {
     fprintf(stderr, "%s", s);
@@ -14,16 +14,9 @@ void *Malloc(size_t n)
     return p;
 }
 
-void sleep_ms(int milliseconds)
-{
-    #ifdef WIN32
-        Sleep(milliseconds);
-    #elif _POSIX_C_SOURCE >= 199309L
-        struct timespec ts;
-        ts.tv_sec = milliseconds / 1000;
-        ts.tv_nsec = (milliseconds % 1000) * 1000000;
-        nanosleep(&ts, NULL);
-    #else
-        usleep(milliseconds * 1000);
-    #endif
+void sleep_ms(int milliseconds) {
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
 }
