a2801c88dc260e0189941795040ba9990b8d58e7
olly-evans <evansoliver345@gmail.com>
2026-09-14 11:28:33 +0100

    test_abuf.c passing two tests and no memory leaks

--- test/test_abuf.c
+++ test/test_abuf.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <string.h>
 
 #include "abuf.h"
 
@@ -8,14 +9,31 @@ void test_abuf_init() {
     assert(t_ab.b == NULL);
     assert(t_ab.len == 0);
 
+    abFree(&t_ab);
 }
 void test_ab_append() {
-    return;
+
+    struct abuf t_ab = ABUF_INIT;
+
+    abAppend(&t_ab, "abcd", 4);
+
+    assert(t_ab.b != NULL);
+    assert(strcmp(t_ab.b, "abcd") == 0);
+    assert(t_ab.len == 4);
+
+    abAppend(&t_ab, "efg", 3);
+
+
+    assert(strcmp(t_ab.b, "abcdefg") == 0);
+    assert(t_ab.len == 7);
+
+    abFree(&t_ab);
 }
 
 int main(void) {
     
     test_abuf_init();
     test_ab_append();
+    
     return 0;
 }
